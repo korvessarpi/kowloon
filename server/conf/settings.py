f3bf004e234fc34c0900d7af4db5ed5a379a0ddc
korvessarpi <korvessawatcher@gmail.com>
2025-11-10 07:57:52 -0600

    Add setting to disable new account registration

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -30,3 +30,5 @@ try:
     from server.conf.secret_settings import *
 except ImportError:
     print("secret_settings.py file not found or failed to import.")
+
+NEW_ACCOUNT_REGISTRATION_ENABLED = False
